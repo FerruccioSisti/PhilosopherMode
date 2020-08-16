3f5d75a7e6d3245f004d8e1bc9bef77f56395e02
FerruccioSisti <ferrucciosisti@gmail.com>
2020-08-16 19:23:59 -0400

    added about page

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,9 +2,10 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $:'jQquery',
+    $:'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
-}))
+    })
+)
 
 module.exports = environment
