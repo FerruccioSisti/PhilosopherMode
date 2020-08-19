2a29e72786226b420736348b22e8f4ee7adffebd
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-08-18 22:04:30 -0400

    messing around trying to get user to work properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
 
     helper_method :is_admin!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    # before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:team, :playername, :email, :password) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:team, :playername, :email, :password, :current_password) }
-    end
+    # protected
+    # def configure_permitted_parameters
+    #     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:team, :playername, :email, :password) }
+    #     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:team, :playername, :email, :password, :current_password) }
+    # end
 
 
     private 
